dd7b713405b6ed20a289ca586dc925449a3f173f
Dondon <kczenlai@gmail.com>
2025-09-11 18:59:14 +0800

    t

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,13 +6,16 @@ module.exports = {
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
       env_local: {
-        APP_ENV: 'local' // APP_ENV=local
+        APP_ENV: 'local', // APP_ENV=local
+        PORT: 3005 // Set your desired port for local
       },
       env_dev: {
-        APP_ENV: 'dev' // APP_ENV=dev
+        APP_ENV: 'dev', // APP_ENV=dev
+        PORT: 4005 // Set your desired port for dev
       },
       env_prod: {
-        APP_ENV: 'prod' // APP_ENV=prod
+        APP_ENV: 'prod', // APP_ENV=prod
+        PORT: 3002 // Set your desired port for prod
       }
   }]
 }
