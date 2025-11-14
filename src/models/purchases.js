334205b8344f5d4e84ed6d6ffebd1a9a5809a828
Dondon Pogi <kczenlai@gmail.com>
2025-11-14 19:13:23 +0800

    t

---
+++ src/models/purchases.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const purchasesSchema = new Schema({
+    member_id: { type: Schema.Types.ObjectId, ref: 'member' },
+    account_type: { type: Number, default: 0 },
+    transdate: { type: Date, default: null },
+    transtime: { type: Date, default: null },
+    order_id: { type: Schema.Types.ObjectId, ref: 'order_header' },
+    product_id: { type: Schema.Types.ObjectId, ref: 'product' },
+    productname: { type: 'String', default: '' },
+    price: { type: 'String', default: '' },
+    discountedprice: { type: Number, default: 0 },
+    qty: { type: Number, default: 0 },
+    discount: { type: Number, default: 0 },
+    subtotal: { type: Number, default: 0 },
+    productcode: { type: 'String', default: '' },    
+    transtype: { type: Number, default: 0 },
+    rbp: { type: Number, default: 0 },
+    mp: { type: Number, default: 0 },
+});
+
+export default mongoose.models.purchase || mongoose.model("purchase", purchasesSchema);
+
